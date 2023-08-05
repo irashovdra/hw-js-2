9ac33fd8967d6b51cc1fc08f8c083d8a6b534659
irashovdra <109846925+irashovdra@users.noreply.github.com>
2023-08-05 12:15:28 -0500

    corrected mistakes

--- exercise5.js
+++ exercise5.js
@@ -1,5 +1,6 @@
-const studying = "Я вивчаю мову програмування";
+const studying = "Я вивчаю мову програмування ";
 console.log(studying);
 
 const program = "JavaScript";
 console.log(program);
+console.log(studying + program);
